0428d95cec039ae5bcc6b0153465ca561a7b8fb3
Shahriyar Mammadli <smammadli@pasha-holding.ph>
2020-08-22 21:51:21 +0400

    SVM, RF, GB are used to build a model

--- helperFunctions.py
+++ helperFunctions.py
@@ -3,12 +3,15 @@
 # Import required libraries
 import pandas as pd
 from sklearn.metrics import accuracy_score
-
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
 # Encode dataframe
 def encodeDf(df, vars):
     for var in vars:
         # Join the encoded df
-        df = df.join(pd.get_dummies(df[var], prefix=var, drop_first=True))
+        df = df.join(pd.get_dummies(df[var], prefix=var))
     # Drop columns as they are now encoded
     return df.drop(vars, axis=1)
 
@@ -19,3 +22,96 @@ def analyzeTicket(df):
 # Confusion Matrix
 def calculateAccuracy(actual, predicted):
     return accuracy_score(actual, predicted)
+
+# Modelling using SVM
+def svmModel(trainDf, testDf, targetVar):
+    clf = svm.SVC(kernel='linear')
+    # Fit data
+    clf = clf.fit(trainDf.drop(targetVar, 1), trainDf[targetVar])
+    return clf, clf.predict(testDf.drop(targetVar, 1))
+
+# Random Forest Model
+def rfModel(trainDf, testDf, targetVar):
+    rf = RandomForestClassifier(n_estimators=100)
+    # Fit data
+    rf = rf.fit(trainDf.drop(targetVar, 1), trainDf[targetVar])
+    return rf, rf.predict(testDf.drop(targetVar, 1))
+
+# Gradient Boosting Model
+def gbModel(trainDf, testDf, targetVar):
+    gb = GradientBoostingClassifier(n_estimators=100, max_features='sqrt')
+    # Fit data
+    gb = gb.fit(trainDf.drop(targetVar, 1), trainDf[targetVar])
+    return gb, gb.predict(testDf.drop(targetVar, 1))
+
+# Number to Categories for Age
+def categorizeAge(df):
+    return df["Age"].map(lambda i: toAgeGroup(i))
+
+# Assign age group
+def toAgeGroup(age):
+    if pd.isnull(age):
+        return age
+    elif int(age) <= 12:
+        return "0-12"
+    elif int(age) > 12 and int(age) <= 17:
+        return "13-24"
+    elif int(age) > 17 and int(age) <= 24:
+        return "18-24"
+    elif int(age) > 24 and int(age) <= 34:
+        return "25-34"
+    elif int(age) > 34 and int(age) <= 44:
+        return "35-44"
+    elif int(age) > 44 and int(age) <= 54:
+        return "45-54"
+    elif int(age) > 54 and int(age) <= 64:
+        return "55-64"
+    elif int(age) > 64 and int(age) <= 74:
+        return "65-74"
+    elif int(age) > 74:
+        return "75+"
+    else:
+        return ""
+
+# Build a model to predict age
+def predictAge(df):
+    # Categorizing the age
+    df['Age'] = categorizeAge(df)
+    # Drop Survived variable to avoid bias
+    dataTemp = df[df['Age'].notna()].drop('Survived', 1)
+    # To assess the model split non-NA samples as train and test
+    X_train, X_test, y_train, y_test = train_test_split(dataTemp.drop(['Age'], 1), dataTemp['Age'],
+                                                        test_size=0.3,
+                                                        random_state=186)
+    X_train['Age'] = y_train
+    X_test['Age'] = y_test
+    model, predicted = rfModel(X_train, X_test, 'Age')
+    pd.DataFrame({'act': y_test, 'pred': predicted}).to_csv("AgeActualvsPredicted.csv", header=True, index=False)
+    # for row in df.itertuples(index=True, name='Pandas'):
+    #     if(pd.isnull(row.Age)):
+    #         print(model.predict(row.drop(['Age', 'Survived'], 1)))
+    return df.apply(lambda i: updateSample(i, model) if pd.isnull(i['Age']) else i, axis=1)
+
+# Update sample by predicting Age
+def updateSample(i, model):
+    i['Age'] = model.predict(i.drop(['Survived', 'Age']).values.reshape(1, -1))[0]
+    return i
+
+# Voting Results i.e. Ensemble result of models
+def ensembleRes(df):
+    return df.apply(lambda i: helperER(i), axis=1)
+
+# Helper function for ensembleRes() funciton
+def helperER(i):
+    # Take the median value which will give the result of most frequent value
+    i['Ensemble'] = sorted(i.values[0:3])[1]
+    return i
+
+# Processing the name and extracting the titles
+def processName(df):
+    return df['Name'].apply(lambda i: i.split(", ")[1].split(" ")[0])
+
+# Processing Cabin by taking the first charachters from the Cabin names which...
+# ...implies the categories. For NAs we will use O as Other
+def processCabin(df):
+    return df['Cabin'].apply(lambda i: "O" if pd.isnull(i) else i[0])
--- solution.py
+++ solution.py
@@ -2,14 +2,11 @@
 # Kaggle Titanic competition solution
 # Import required libraries
 import pandas as pd
-from sklearn import svm
 import helperFunctions as hf
 
 # Train dataframe data preparation
 trainDf = pd.read_csv('data/train.csv')
 # See the columns in the dataframe
-print("-------------- Column names: --------------")
-print(trainDf.columns)
 print("------------------ Head: ------------------")
 print(trainDf.head())
 # Return the number of NAs for each column
@@ -18,30 +15,12 @@ print(trainDf.isna().sum())
 # The number of unique values for each column
 print("--------- Number of unique values: ---------")
 print(trainDf.nunique())
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
 # Embarked has only two missing values, so drop NAs for that column
 trainDf = trainDf[trainDf['Embarked'].notna()]
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
-# Cabin has too many missing values, so drop that column
-trainDf.drop('Cabin', axis=1, inplace=True)
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
-# Drop NAs for age column
-trainDf = trainDf[trainDf['Age'].notna()]
 # Drop id and name columns
-trainDf.drop(['PassengerId', 'Name', 'Fare', 'Age'], axis=1, inplace=True)
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
+trainDf = trainDf.drop(['PassengerId'], axis=1)
 # Process Ticket since it has different prefixes for different types
 trainDf[["Ticket"]] = hf.analyzeTicket(trainDf[["Ticket"]])
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
-# Encode dataframe
-# trainDf = hf.encodeDf(trainDf, ['Sex', 'Pclass', 'SibSp', 'Parch', 'Embarked', 'Ticket'])
-print("-------------- Column names: --------------")
-print(trainDf.columns)
 
 # Test dataframe data preparation
 testDf = pd.read_csv('data/test.csv')
@@ -49,48 +28,42 @@ originalDf = testDf
 # Read actual results
 actualDf = pd.read_csv('data/gender_submission.csv')
 testDf = pd.merge(testDf, actualDf, left_on='PassengerId', right_on='PassengerId')
-print("------------------ Shape: ------------------")
-print(testDf.shape)
 print("-------------- Number of NAs: --------------")
 print(testDf.isna().sum())
-# Embarked has one missing values, so drop NAs for that column
-#testDf = testDf[testDf['Fare'].notna()]
 # Drop id and name columns, Cabin
-testDf.drop(['PassengerId', 'Name', 'Cabin', 'Fare', 'Age'], axis=1, inplace=True)
-# Drop NAs for age column
-# testDf = testDf[testDf['Age'].notna()]
-print("------------------ Shape: ------------------")
-print(testDf.shape)
+testDf = testDf.drop(['PassengerId'], axis=1)
 # Process Ticket since it has different prefixes for different types
 testDf[["Ticket"]] = hf.analyzeTicket(testDf[["Ticket"]])
-print("------------------ Shape: ------------------")
-print(testDf.shape)
-# Encode dataframe
-# testDf = hf.encodeDf(testDf, ['Sex', 'Pclass', 'SibSp', 'Parch', 'Embarked', 'Ticket'])
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
-print("------------------ Shape: ------------------")
-print(testDf.shape)
 # Since some of the categorical values may exist in test but not in train...
 # ...before encoding they needed to ber merged, and splitting again after encoding
 mergedDf = pd.concat([trainDf, testDf],ignore_index=True)
-mergedDf = hf.encodeDf(mergedDf, ['Sex', 'Pclass', 'SibSp', 'Parch', 'Embarked', 'Ticket'])
+# Process Names and extract titles from them
+mergedDf['Name'] = hf.processName(mergedDf)
+# Fare has single missing value fill it by mean value
+mergedDf.loc[mergedDf.loc[pd.isna(mergedDf['Fare']), :].index[0], 'Fare'] = testDf['Fare'].mean()
+# Cabin has missing values these missing values will be filled
+mergedDf['Cabin'] = hf.processCabin(mergedDf)
+mergedDf = hf.encodeDf(mergedDf, ['Sex', 'Pclass', 'SibSp', 'Parch', 'Embarked', 'Ticket', 'Name', 'Cabin'])
+# Convert age into categories
+mergedDf = hf.predictAge(mergedDf)
+mergedDf = hf.encodeDf(mergedDf, ['Age'])
 trainDf = mergedDf[0:trainDf.shape[0]]
 testDf = mergedDf[trainDf.shape[0]:mergedDf.shape[0]]
-print("------------------ Shape: ------------------")
-print(trainDf.shape)
-print("------------------ Shape: ------------------")
-print(testDf.shape)
 
 # Modelling
 # SVM model
 # Initialize SVM classifier
-clf = svm.SVC(kernel='linear')
-# Fit data
-clf = clf.fit(trainDf.drop('Survived', 1), trainDf[['Survived']])
-predicted = clf.predict(testDf.drop('Survived', 1))
-originalDf[['Survived']] = predicted
-print(hf.calculateAccuracy(testDf['Survived'],predicted))
+modelSVM, predictedSVM = hf.svmModel(trainDf, testDf, 'Survived')
+# Random Forest model
+modelRF, predictedRF = hf.rfModel(trainDf, testDf, 'Survived')
+# Gradient Boosting model
+modelGB, predictedGB = hf.gbModel(trainDf, testDf, 'Survived')
+ensembleDf = pd.DataFrame({'SVM': predictedSVM, 'RF': predictedRF, 'GB': predictedGB, 'Ensemble': None})
+ensembleDf = hf.ensembleRes(ensembleDf)
+# Votes of models
+ensembleDf.to_csv("ensembleRes.csv", header=True, index=False)
+originalDf[['Survived']] = predictedGB
 originalDf[['PassengerId', 'Survived']].to_csv("submission.csv", header=True, index=False)
-# TODO: user Age and Fare
-# TODO: Do ensemble
+
+
+# TODO: Use embarked
